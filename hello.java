b846bcd3844bd122f521ca9ffe5fa1904d8a78f9
KausaChangs <changayakausa@gmail.com>
2023-06-16 09:48:20 +0200

    my first commit

---
+++ hello.java
@@ -0,0 +1,35 @@
+
+import java.util.*;
+
+public class JavaLearn{
+
+
+
+   public static void main(String[] args){
+
+   
+  ArrayList<String> nums = new ArrayList<String>();
+  
+  nums.add("One");
+  nums.add("Two");
+  nums.add("Three");
+  nums.add("Four");
+  System.out.println(nums.toString());
+
+   
+
+    
+  ArrayList<Employee> empList = new ArrayList<Employee>();
+
+  Employee e = (Employee)emplist.get(0); //cast the to Employee type
+
+  
+
+
+
+ }
+
+   
+
+
+}
