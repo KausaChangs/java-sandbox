7e4eee8808e916261b0903674c70aa7afd91ffc0
KausaChangs <changayakausa@gmail.com>
2023-06-16 09:57:42 +0200

    wrote a small prices app

---
+++ Prices.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Prices {
+
+	public static void main(String args[]){
+
+
+		HashMap<String, Double> priceList = new HashMap<String, Double>();
+
+		priceList.put("Hammer", 10.99);
+
+
+		Double hammerPrice = priceList.get("Hammer");
+
+
+		if(hammerPrice != null){
+			
+			System.out.println("A hammer costs " + hammerPrice);
+
+		} 
+           else{
+			System.out.println("We don't sell hammers.");
+		
+		}
+
+	
+    }
+
+
+	}
