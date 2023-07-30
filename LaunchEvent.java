891ded9015a17d1ba3abf98638b736b40418ed32
KausaChangs <changayakausa@gmail.com>
2023-07-30 16:02:50 +0200

    created a LaunchEvent class to test out the runnable implementation

---
+++ LaunchEvent.java
@@ -0,0 +1,26 @@
+public class LaunchEvent implements Runnable {
+
+    private int start;
+    private String message;
+
+    public LaunchEvent(int start, String message) {
+        this.start = start;
+        this.message = message;
+    }
+
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(20000 - (start * 1000));
+        } catch (InterruptedException e) {
+            // Handle the exception, if needed
+        }
+        System.out.println(message);
+    }
+
+    public static void main(String[] args) {
+        LaunchEvent launchEvent = new LaunchEvent(10, "Launch in 10 Seconds!");
+        Thread thread = new Thread(launchEvent);
+        thread.start();
+    }
+}
