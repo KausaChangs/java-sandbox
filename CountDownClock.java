d28fb1c477a2830ef0c39e70a0239475440cb2f9
KausaChangs <changayakausa@gmail.com>
2023-07-30 10:15:09 +0200

    implemented my first Java thread

---
+++ CountDownClock.java
@@ -0,0 +1,21 @@
+public class CountDownClock implements Runnable {
+
+    @Override
+    public void run() {
+        for (int t = 20; t >= 0; t--) {
+            System.out.println("T minus " + t);
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                // Handle the exception, if needed
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        CountDownClock countDownClock = new CountDownClock();
+        Thread thread = new Thread(countDownClock);
+        thread.start();
+    }
+}
