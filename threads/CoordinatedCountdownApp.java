280a88d3161cf07f964bf86ebd8b0eec410ce605
KausaChangs <changayakausa@gmail.com>
2023-08-01 14:43:13 +0200

    need to learn how to run coordinated threads

--- threads/CoordinatedCountdownApp.java
+++ threads/CoordinatedCountdownApp.java
@@ -1,50 +1,76 @@
+//need to fix this
+
 import java.util.ArrayList;
-// version 2.0 of the Countown application
 
-public class CoordinatedCountdownApp{
-    public static void main(String[] args){
-       CountDownClock clock = new CountDownClock(20);
 
-       ArrayList<Runnable> events = new ArraList<Runnable>();
+public class CoordinatedCountdownApp {
+    public static void main(String[] args) {
+         
+        CoordinatedCountdownApp app = new CoordinatedCountdownApp();
+        CountDownClock clock = app.new CountDownClock(20);
 
-       events.add(new LaunchEvent(16, "Flood the pad!", clock));
-       events.add(new LaunchEvent(6, "Start the engines", clock));
-       events.add(new LaunchEvent(0, "LiftOff!", clock));
+        ArrayList<Runnable> events = new ArrayList<>();
 
-       clock.start();
+        events.add(clock.new LaunchEvent(16, "Flood the pad!"));
+        events.add(clock.new LaunchEvent(6, "Start the engines"));
+        events.add(clock.new LaunchEvent(0, "LiftOff!"));
 
-       for(Runnable e : events){
-        new Thread(e).start();
-       }
+        clock.start();
+
+        for (Runnable e : events) {
+            new Thread(e).start();
+        }
     }
 
-    interface TimeMonitor{
+    interface TimeMonitor {
         int getTime();
     }
 
-    class CountDownClock extends Thread implements TimeMonitor{
+    class CountDownClock extends Thread implements TimeMonitor {
         private int t;
 
-        public CountDownClock(int start){
+        public CountDownClock(int start) {
             this.t = start;
         }
 
-        public void run(){
-            for (; t >= 0; t--){
-                System.out.println("T minus "+ t);
-                try{
+        public void run() {
+            for (; t >= 0; t--) {
+                System.out.println("T minus " + t);
+                try {
                     Thread.sleep(1000);
-                }
-                catch(InterruptedExcepton e){
-                    
+                } catch (InterruptedException e) {
+                    // Handle InterruptedException (e.g., print an error message)
                 }
             }
         }
 
-        public int getTime(){
+        public int getTime() {
             return t;
         }
+
+        class LaunchEvent implements Runnable {
+            private int start;
+            private String message;
+
+            public LaunchEvent(int start, String message) {
+                this.start = start;
+                this.message = message;
+            }
+
+            public void run() {
+                boolean eventDone = false;
+                while (!eventDone) {
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        // Handle InterruptedException (e.g., print an error message)
+                    }
+                    if (getTime() <= start) {
+                        System.out.println(this.message);
+                        eventDone = true;
+                    }
+                }
+            }
+        }
     }
-
-
 }
